11d09012c54a6e8c3d77b2a84e789869f2cfad6b
RyanLawWilson <59398576+RyanLawWilson@users.noreply.github.com>
2020-02-25 11:57:50 -0700

    Add project files.

---
+++ First C-Sharp Progam in Visual Studio/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace First_C_Sharp_Progam_in_Visual_Studio
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("What is your name?");            
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
+        }
+    }
+}
